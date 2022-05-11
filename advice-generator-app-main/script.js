f3596481460ff9f01e747ccd1d1dc3e438773e7d
unknown <adrian.prip@yahoo.com>
2022-05-11 18:06:10 +0300

    added tech stack to menu items

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,17 +1,17 @@
-const adviceNumber = document.getElementById('advice-number');
-const advice = document.getElementById('advice');
-const button = document.getElementById('button');
-
+const adviceNumber = document.getElementById("advice-number");
+const advice = document.getElementById("advice");
+const button = document.getElementById("button");
 
 const fetchAdvice = async () => {
-    const result = await fetch("https://api.adviceslip.com/advice");
-    const data = await result.json();
-    adviceNumber.innerText = `Advice #${data.slip.id}`;
-    advice.innerText = `"${data.slip.advice}"`
- }
- fetchAdvice();
- button.addEventListener('click' , () =>{
-    fetchAdvice();
- })
- 
+  const result = await fetch("https://api.adviceslip.com/advice");
+  const data = await result.json();
 
+  adviceNumber.innerText = `Advice #${data.slip.id}`;
+  advice.innerText = `"${data.slip.advice}"`;
+};
+
+fetchAdvice();
+
+button.addEventListener("click", () => {
+  fetchAdvice();
+});
--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 const challenges = [
-    'advice-generator-app-main',
-    'base-apparel-coming-soon-master',
-    'four-card-feature-section-master',
-    'fylo-landing-page',
-    'huddle-landing-page',
-    'rock-paper-scissors',
-    'single-price-grid-component',
-    'ping-coming-soon-page',
-    'nft-preview-card-component',
-    'intro-component-with-signup-form'
+  ["advice-generator-app-main", "HTML CSS JS API"],
+  ["base-apparel-coming-soon-master", "HTML CSS JS"],
+  ["four-card-feature-section-master", "HTML CSS"],
+  ["fylo-landing-page", "HTML CSS"],
+  ["huddle-landing-page", "HTML CSS"],
+  ["rock-paper-scissors", "HTML CSS JS"],
+  ["single-price-grid-component", "HTML CSS"],
+  ["ping-coming-soon-page", "HTML CSS JS"],
+  ["nft-preview-card-component", "HTML CSS"],
+  ["intro-component-with-signup-form", "HTML CSS JS"],
 ];
 
 const challengesList = document.getElementById("challengesList");
 
-challenges.forEach(challenge => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = `./${challenge}/index.html`;
-    link.innerText = formatChallengeName(challenge);
+challenges.forEach((challenge) => {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = `./${challenge[0]}/index.html`;
+  link.innerText = formatChallengeName(challenge[0]);
 
-    const img = document.createElement('img');
-    img.src = `${challenge}/design/desktop-design.jpg`;
+  const img = document.createElement("img");
+  img.src = `${challenge[0]}/design/desktop-design.jpg`;
 
-    link.prepend(img);
-    listItem.appendChild(link);
-    challengesList.appendChild(listItem);
+  const tech = document.createElement("p");
+  tech.innerText = challenge[1];
+
+  link.append(tech);
+  link.prepend(img);
+  listItem.appendChild(link);
+  challengesList.appendChild(listItem);
 });
 
-function formatChallengeName(name){
-    return name
-    .split('-')
-    .map(word => word[0].toUpperCase() + word.slice(1))
-    .join(" ")
+function formatChallengeName(name) {
+  return name
+    .split("-")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
